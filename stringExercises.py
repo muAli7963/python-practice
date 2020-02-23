a17e26c953dc038485af0123652528538b45bc47
Ben_Ali <ali.muhhamad745@gmail.com>
2020-02-23 21:31:55 +0000

    new challlenges added

--- stringExercises.py
+++ stringExercises.py
@@ -106,4 +106,130 @@ print(to_uppercase('Python'))
 print(to_uppercase('PyThon'))
 
 
+#Write a python program to count repeated characters in a string.
+
+import collections
+str1 = 'thequickbrownfoxjumpsoverthelazydog'
+d = collections.defaultdict(int)
+for c in str1:
+    d[c] += 1
+
+for c in sorted(d, key=d.get, reverse=True):
+  if d[c] > 1:
+      print(f'{c}, {d[c]}')
+
+
+#Write a Python program to find the first non-repeating character in given string.
+
+def first_non_repeating_character(str1):
+  char_order = []
+  ctr = {}
+  for c in str1:
+    if c in ctr:
+      ctr[c] += 1
+    else:
+      ctr[c] = 1 
+      char_order.append(c)
+  for c in char_order:
+    if ctr[c] == 1:
+      return c
+  return None
+
+print(first_non_repeating_character('abcdef'))
+print(first_non_repeating_character('abcabcdef'))
+print(first_non_repeating_character('aabbcc'))
+
+#Write a Python program to find the first repeated character in a given string.
+
+def first_repeated_char(str1):
+  for index,c in enumerate(str1):
+    if str1[:index+1].count(c) > 1:
+      return c 
+  return "None"
+
+print(first_repeated_char("abcdabcd"))
+print(first_repeated_char("abcd"))
+
+#Write a Python program to find the first repeated word in a given string.
+
+def first_repeated_word(str1):
+  temp = set()
+  for word in str1.split():
+    if word in temp:
+      return word;
+    else:
+      temp.add(word)
+  return 'None'
+print(first_repeated_word("ab ca bc ab"))
+print(first_repeated_word("ab ca bc ab ca ab bc"))
+print(first_repeated_word("ab ca bc ca ab bc"))
+print(first_repeated_word("ab ca bc"))
+
+
+#Write a Python program to find the second most repeated word in a given string.
+
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    counts_x = sorted(counts.items(), key=lambda kv: kv[1])
+    #print(counts_x)
+    return counts_x[-2]
+ 
+print(word_count("Both of these issues are fixed by postponing the evaluation of annotations. Instead of compiling code which executes expressions in annotations at their definition time, the compiler stores the annotation in a string form equivalent to the AST of the expression in question. If needed, annotations can be resolved at runtime using typing.get_type_hints(). In the common case where this is not required, the annotations are cheaper to store (since short strings are interned by the interpreter) and make startup time faster."))
+
+
+#Write a Python program to compute sum of digits of a given string.
+
+def sum_digits_string(str1):
+    sum_digit = 0
+    for x in str1:
+        if x.isdigit() == True:
+            z = int(x)
+            sum_digit = sum_digit + z
+
+    return sum_digit
+     
+print(sum_digits_string("123abcd45"))
+print(sum_digits_string("abcd1234"))
+
+
+#Write a Python program to make two given strings (lower case, may or may not be of the same length) anagrams removing any characters from any of the strings.
+
+def make_map(s):
+    temp_map = {}
+    for char in s:
+        if char not in temp_map:
+            temp_map[char] = 1
+        else:
+            temp_map[char] +=1 
+    return temp_map        
+def make_anagram(str1, str2):
+    str1_map1 = make_map(str1)
+    str2_map2 = make_map(str2)
+ 
+    ctr = 0
+    for key in str2_map2.keys():
+        if key not in str1_map1:
+            ctr += str2_map2[key]
+        else:
+            ctr += max(0, str2_map2[key]-str1_map1[key])
+ 
+    for key in str1_map1.keys():
+        if key not in str2_map2:
+            ctr += str1_map1[key]
+        else:
+            ctr += max(0, str1_map1[key]-str2_map2[key]) 
+    return ctr 
+str1 = input("Input string1: ")
+str2 = input("Input string2: ")
+print(make_anagram(str1, str2))
+
+
 
